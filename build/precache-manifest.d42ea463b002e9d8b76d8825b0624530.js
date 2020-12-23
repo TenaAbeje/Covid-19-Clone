9151b8f3633c1907c6876d45603540d9a5f2980d
Tena <73759195+TenaAbeje@users.noreply.github.com>
2020-12-23 02:08:25 -0800

    updated

---
+++ build/precache-manifest.d42ea463b002e9d8b76d8825b0624530.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa61f8ba66d21dfe2b126f640df4fda6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "740498ca067f2ba1b143",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "9943b9e7bf82a926547d",
+    "url": "/static/css/main.d5a661b2.chunk.css"
+  },
+  {
+    "revision": "740498ca067f2ba1b143",
+    "url": "/static/js/2.a0a89fbe.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.a0a89fbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9943b9e7bf82a926547d",
+    "url": "/static/js/main.933fc9f5.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
